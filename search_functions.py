40eb4567ea2daaee094d2056a2da401ba47b0d8a
Gabriel Barnett <gabrielcbarnett@gmail.com>
2023-03-27 18:50:56 +1100

    Updated wording in the files and fixed some formatting.
    User now gets instructions on how to use the software every time it's run.

--- search_functions.py
+++ search_functions.py
@@ -1,42 +1,19 @@
 """
-myKamus by Gabriel Barnett
-
-myKamus is An open source instant translation software for Indonesian that provides the user
-with complex Indonesian-English translation capabilities.
-
-To run the program you cna either do it from inside an IDE of your choice, or with Python installed either:
-    a) Run clipboard_monitor through IDLE
-    b) Launch a Powershell session through the directory and run clipboard_monitor through it
-
-It utilises several open source bitext corpus to provide access to over 50 million example sentences and words for
-the purposes of translation.
-
-The program is free to use for academic and non-commercial applicaitons, if you wish to use it for something else
-email me at gabrielcbarnett@gmail.com. There will be no cost involved, it is so we can discuss any needs you might have
-for updates, specific vocabulary or language requirements. Again, it will be free but a representative from your
-organisation must make contact with me first.
-
-If you like this program and have found it useful for your work, feel free to email with your success story or any
-improvements that you might suggest.
+Module contains the functions necessary to search through the databases provided byt the user.
 """
 
-print("Loading...")
+print("myKamus is loading...\n")
 
 with open('en-id_dict.txt', encoding="utf-8") as dic:
     dictionary = dic.readlines()
 
 with open('en-id_sentences.txt', encoding='utf-8') as sentences:
     sentences = sentences.readlines()
-
-print("Finished loading")
-print("Welcome to myKamus")
-print("With help from:")
-print("P. Lison and J. Tiedemann, 2016, OpenSubtitles2016: Extracting Large Parallel Corpora from Movie and TV "
-      "Subtitles. ""In Proceedings of the 10th International Conference on "
-      "Language Resources and Evaluation (LREC 2016)")
-
-
 def search_for_word():
+    """
+    Not currently in use, defined for testing purposes and the
+    :return: string(s)
+    """
     print("We are ready to take your word, please type it below:")
     user_input = input()
     de_capitalised = user_input.lower()
